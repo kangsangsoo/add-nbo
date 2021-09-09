744b257adb20a2cb380ef84cc58b5ed2cd3243e6
kangsangsoo <rkdtkdtn0706@korea.ac.kr>
2021-09-09 18:51:22 +0900

    fread의 인자 수정

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -3,21 +3,16 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
-	uint8_t c1[4], c2[4];
 	FILE* fp1 = fopen(argv[1], "r");
 	FILE* fp2 = fopen(argv[2], "r");
 
-	fread(c1, 4, 4, fp1);
-	fread(c2, 4, 4, fp2);
+	uint32_t a1, a2;
+	fread(&a1, 4, 1, fp1);
+	fread(&a2, 4, 1, fp2);
 
-	uint32_t a1 = 0, a2 = 0;
-
-	for (int i = 0; i < 4; i++) {
-		a1 += c1[i] << (8 * i);
-		a2 += c2[i] << (8 * i);
-	}
 	a1 = ntohl(a1);
 	a2 = ntohl(a2);
+
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", a1, a1, a2, a2, a1 + a2, a1 + a2);
 	
 	fclose(fp1);
